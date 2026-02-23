2623261c25b7c6c3e436b4a97b1c67d08a727c2c
Aadi Savan Vinod <aadisavanvinod@gmail.com>
2026-02-23 14:47:47 +0530

    Aadi added UC2

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,5 +1,6 @@
-public class PalindroneChecke {
+public class PalindroneChecker {
 public static void main(String[] args){
 System.out.println("Welcome to Palindrome Checker App Management System");
+System.out.println("Modify this logic for usecase2");
 }
 }
